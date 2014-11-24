219a18418bc51758a64addaf78b47e49452b8c3d
carshelves <abdelselouane@gmail.com>
2014-11-24 15:09:55 -0500

    cache memory issue fixed

---
+++ js/script.js
@@ -0,0 +1,156 @@
+    var editor = CodeMirror.fromTextArea(document.getElementById("demotext"), {
+	      lineNumbers: true,
+	      mode: "application/ld+json",
+	      theme: "paraiso-dark",
+	      indentUnit: 1,
+	      smartIndent: true,
+	      tabSize: 3,
+	      lineWrapping: true,
+	      showCursorWhenSelecting: true,
+	      historyEventDelay: 10,
+
+	      
+	    });
+        
+        $(document).ready(function() {
+            
+            $.getScript( "js/data/users.js", function( data, textStatus, jqxhr ) {
+                editor.setValue( data );
+            });
+            
+            var getContent = function( users ){
+                if(users){
+                    var content = '';
+                    for(var i=0; i < users.length; i++){
+                        content +=  "<tr><td>#"+(i+1)+"</td>";
+                        for (key in users[i]) {
+                            content += "<td data-id='"+i+"' data-name='"+key+"' class='edit'>"+users[i][key]+"</td>";
+                        }
+                        content +=  "<td>&nbsp;<a href='#' class='preview' data-id='"+i+"' data-toggle='modal' data-target='#myModal'><span data-id='"+i+"' class='glyphicon glyphicon-eye-open'></span></a>&nbsp;<a class='update' data-id='"+i+"' data-toggle='modal' data-target='#myModal' href='#'><span data-id='"+i+"' class='glyphicon glyphicon-edit'></span></a>&nbsp;<a href='#'><span data-id='"+i+"' class='glyphicon glyphicon-trash'></span></a>&nbsp;</td></tr>";
+                    }
+                    return content;
+                }
+                return false;
+            }
+            
+            var loadContent = function( users ){
+                $('#table-content').html(getContent(users));
+            }
+            
+           
+            var saveUsers = function( obj ){
+            	
+                $.ajax({
+                  url: "users.php",
+                  type: "POST",
+                  data: { users : obj },
+                  success: function( response ){
+                      if( response == 'success' ) window.location.reload(true); 
+                  },
+		          error: function(error){
+		      		  console.log( error ); return false;
+		          }    
+                });
+            }
+            
+            var getUser = function( users, id ){
+                if(users)
+                    return users[id];
+                else
+                    return false;
+            }
+            
+           if(users && users.length !== 0){
+            
+                loadContent( users );
+                var tableObj = $('#user_listing');
+			    tableObj.dataTable().editableTableWidget();
+           }
+           
+            
+            $('#user_submit').click(function(){
+                var id = this.getAttribute('data-id');
+                var action = this.getAttribute('data-action');
+                               
+                var formObj = {};
+                var inputs = $('#user_form').serializeArray();
+                
+                $.each(inputs, function (i, input) {
+                    formObj[input.name] = input.value;
+                });
+                
+                if( action == 'add'){
+                     users.push(formObj);
+                }else{
+                    users[id].name = formObj.name;
+                    users[id].phone = formObj.phone;
+                    users[id].title = formObj.title;
+                    users[id].office = formObj.office;
+                }
+                 saveUsers( users );
+            });
+        
+            $('.edit').on('change', function(evt, newValue) {
+                
+                var id = this.getAttribute('data-id');
+                var field = this.getAttribute('data-name');
+               
+                if (newValue !== '') {
+                    users[id][field] = newValue;
+                    saveUsers( users );
+                }else{
+                    console.log('can not be empty');
+                }
+            });
+                
+            $('.glyphicon-trash').click(function(){
+                var id = this.getAttribute('data-id');
+                if( users.length > 1){
+                    if (confirm("Do you really want to delete this user, are you sure?")) {
+                         var removedObject = users.splice(id,1);
+                         saveUsers( users );
+                    }
+                }else{
+                    alert("This user is the last registered user, it can not be deleted");
+                }
+                return false;
+            });
+           
+            $('#add_user').click(function(){
+                 $('#myModalLabel').html('<span class="glyphicon glyphicon-user"></span> Add User');
+                 $('#name').val('').removeAttr('disabled');
+                 $('#phone').val('').removeAttr('disabled');
+                 $('#title').val('').removeAttr('disabled');
+                 $('#office').val('').removeAttr('disabled');
+                 $('#user_submit').removeAttr('disabled');
+                 $('#user_submit').attr( "data-id", "" );
+                 $('#user_submit').attr( "data-action", "add" );
+                 $('#myModal').trigger('click');
+            });
+            
+			$('.preview').click(function (event) {
+                var id = this.getAttribute('data-id');
+                var user = getUser(users, id);
+                $('#myModalLabel').html('<span class="glyphicon glyphicon-user"></span> Preview User');
+                $('#name').val(user.name).attr( "disabled", "disabled" );
+                $('#phone').val(user.phone).attr( "disabled", "disabled" );
+                $('#title').val(user.title).attr( "disabled", "disabled" );
+                $('#office').val(user.office).attr( "disabled", "disabled" );
+                $('#user_submit').attr( "disabled", "disabled" );
+                $('#myModal').trigger('click');
+            });
+            
+            $('.update').click(function (event) {
+                var id = this.getAttribute('data-id');
+                var user = getUser(users, id);
+                $('#myModalLabel').html('<span class="glyphicon glyphicon-user"></span> Edit User');
+                $('#name').val(user.name).removeAttr('disabled');;
+                $('#phone').val(user.phone).removeAttr('disabled');;
+                $('#title').val(user.title).removeAttr('disabled');;
+                $('#office').val(user.office).removeAttr('disabled');;
+                $('#user_submit').removeAttr('disabled');
+                $('#user_submit').attr( "data-id", id );
+                $('#user_submit').attr( "data-action", "edit" );
+                $('#myModal').trigger('click');
+            });
+		});
---
+++ users.php
@@ -0,0 +1,37 @@
+<?
+header('Cache-Control: no-cache, no-store, must-revalidate'); // HTTP 1.1.
+header('Pragma: no-cache'); // HTTP 1.0.
+header('Expires: 0'); // Proxies.
+
+if(isset($_POST)){
+
+    //echo '<pre>'; print_r($_POST['users']); echo'</pre>';exit;
+
+    $file = 'js/data/users.js';
+    $handle = fopen($file, 'w') or die('Cannot open file:  '.$file);
+    
+    $document = 'var users = [';
+    
+    foreach( $_POST['users'] as $key => $value){
+        $document .= '{';
+        foreach($value as $k => $val){
+            $document .= $k.':"'.$val.'",';
+        }
+        $document = substr($document, 0, (strlen($document)-1));
+        $document .= '},';
+    }
+
+    $document = substr($document, 0, (strlen($document)-1));
+    $document .= '];';
+   // echo $document;
+    
+    //exit;
+    if(fwrite($handle, $document))
+    	echo 'success';
+    else
+    	echo 'failure';
+    fclose($handle);
+    exit;
+}
+
+?>
